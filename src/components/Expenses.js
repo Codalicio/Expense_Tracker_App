607dbfb2a8ac04dc45bb4f24ebb1e6dbdc1bfc7e
Amit Raj <rajamitnine@gmail.com>
2022-12-24 15:21:50 +0530

    Added a 'Card' container and four 'coustom HTML elements' named 'ExpenseIUtem'

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css';
+import Card from './Card';
 
 const Expenses = (props) => {
 
   return (
-    <div>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,7 +27,7 @@ const Expenses = (props) => {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   )
 }
 
