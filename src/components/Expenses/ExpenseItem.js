cafdd5e0a90b1093a914af43b3cb5da9f201ee65
Amit Raj <rajamitnine@gmail.com>
2022-12-28 22:47:58 +0530

    Added ExpenseFilter.js and did two-way data binding to send data to Expenses.js and also attached a css file for ExpenseFilter.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-  console.log('ExpenseItem evaluated by React');
-
-  const clickHandler = () => {
-    setAmount(234);
-    setTitle('Updated Item');
-  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate newDate={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
